fb49e8f7095bb85c6395ed46455c0dcc91c819d1
ssebinacharles <ssebinacharles50@gmail.com>
2026-07-21 17:12:16 +0000

    Update settings,py:- core

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -141,3 +141,11 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CORS_ALLOW_ALL_ORIGINS = True
+# ==========================================
+# CELERY & REDIS CONFIGURATION
+# ==========================================
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_TIMEZONE = 'UTC'
